0321b672e9cb7cc206b49934fa45538cbd803954
Rameez Mughal <rameezhm@outlook.com>
2018-12-10 01:27:03 -0800

    Work on action page

--- sliders.js
+++ sliders.js
@@ -1,7 +1,7 @@
 //define the sliders and output values for all the sliders
 var slider1 = document.getElementById("slider1");
 var outputValue1 = document.getElementById("slider-val1");
-outputValue1.innerHTML =  slider1.value + "%";
+outputValue1.innerHTML =  "-"+slider1.value/10 + "&#176";
 
 var slider2 = document.getElementById("slider2");
 var outputValue2 = document.getElementById("slider-val2");
@@ -11,9 +11,36 @@ var slider3 = document.getElementById("slider3");
 var outputValue3 = document.getElementById("slider-val3");
 outputValue3.innerHTML =  slider3.value + "%";
 
+var slider4 = document.getElementById("slider4");
+var outputValue4 = document.getElementById("slider-val4");
+outputValue4.innerHTML =  slider4.value + "%";
+
+var slider5 = document.getElementById("slider5");
+var outputValue5 = document.getElementById("slider-val5");
+outputValue5.innerHTML =  slider5.value + "%";
+
+
+//water cup
+var totalWidth = window.innerWidth;
+var waterCupCanvas = document.getElementById("waterCup");
+
+//thermostat
+var thermostat = document.getElementById("thermostat");
+
+var setThermoSize = function() {
+    thermostat.style.height = thermostat.getBoundingClientRect().width+"px";
+}
+setThermoSize();
+//position temperature label
+outputValue1.style.position = "relative";
+outputValue1.style.top = "33%";
+
+
 //add the slider handler functions
 slider1.oninput = function() {
-    outputValue1.innerHTML =  this.value + "%";
+    outputValue1.innerHTML = "-" + (this.value)/10 + "&#176";
+    var val = this.value;
+    thermostat.style.background = "rgb("+(214-2*val)+", 27, "+parseInt(27+parseInt(val))+ ")";
 }
 
 slider2.oninput = function() {
@@ -23,4 +50,19 @@ slider2.oninput = function() {
 slider3.oninput = function() {
     outputValue3.innerHTML =  this.value + "%";
 }
+
+slider4.oninput = function() {
+    outputValue4.innerHTML =  this.value + "%";
+}
+
+slider5.oninput = function() {
+    outputValue5.innerHTML =  this.value + "%";
+}
+
+//resize window function
+onresize = function(){
+    setThermoSize();
+}
+
+
 
